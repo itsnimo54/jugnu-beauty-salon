822d561d182e9229967dacb9c6949fb3bba722fc
itsnimo54 <itsalicehere419@gmail.com>
2026-07-07 21:37:10 -0700

    Update Navbar.tsx

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar({ isDark, toggleDark }: NavbarProps) {
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
-        scrolled
+        scrolled || location.pathname !== '/'
           ? 'bg-luxury-black/95 backdrop-blur-xl shadow-lg'
           : 'bg-transparent'
       }`}
